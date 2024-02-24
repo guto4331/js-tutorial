c5495529eab5556e080b17ac9e11c8e00e169030
guto4331 <guto4331@gmail.com>
2024-02-24 14:13:15 +0900

    add: thumbBar を実装

--- image-gallery/main.js
+++ image-gallery/main.js
@@ -4,15 +4,14 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+const images = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
+const altText = ['Closeup of a human eye', 'Beautiful rock', 'Flowers', 'Ancient mural', 'Moth'];
 
-/* Declaring the alternative text for each image file */
-
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for(i = 0; i < images.length; i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', images[i]);
+    newImage.setAttribute('alt', altText[i]);
+    thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
